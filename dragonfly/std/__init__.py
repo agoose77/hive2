1b1ff0efcb165682db403fe77f570026156520b1
Angus Hollands <goosey15@gmail.com>
2017-06-25 23:07:05 +0100

    Remove call and define nodes (too low level)

--- dragonfly/std/__init__.py
+++ dragonfly/std/__init__.py
@@ -1,5 +1,3 @@
 from .buffer import Buffer
-from .call import Call
-from .define import Define
 from .transistor import Transistor
 from .variable import Variable, VariableBool, VariableInt, VariableFloat, VariableStr
